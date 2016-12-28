1145922538b218408e02fd8fb47a4290071c7b80
Mathias Nohall <mathias.nohall@valtech.se>
2016-12-28 12:44:03 +0100

    added .net core web api

---
+++ src/ThouShallNotPass.Api/Controllers/PasswordGeneratorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThouShallNotPass.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class PasswordGeneratorController : Controller
+    {
+        
+        [HttpGet]
+        [Route("[action]")]
+        public IEnumerable<string> Generate()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        
+    }
+}
